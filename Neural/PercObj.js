d46821d983bbfa58771c18b0448af06db4ef56ce
Luke <lr98lr@aol.com>
2018-06-30 19:19:02 -0400

    remux-- general purpose neural net

--- Neural/PercObj.js
+++ Neural/PercObj.js
@@ -1,9 +1,37 @@
 
-var weights = [2];
-function Perceptron(){
-	//random weights
-	for(var i = 0; i < weights.length; i++)
-		weights.push(random(-1,1));
+function Perceptron(numWeights){
+	var weights = [];
+	this.randomWeights = function(){
+		for(var i = 0; i < numWeights; i++){
+			weights.push(random(-1,1));
+			console.log(weights[i]);
+		}
+
+
+	}
+
+	this.guess = function(inputs){
+		var sum = 0;
+		for(var i = 0; i < weights.length; i++)
+			sum+= inputs[i] * weights[i];
+			//activation function is a function of the sign of the sum
+			var output = sum >= 0 ? 1 : -1;
+			return output;
+	}
+
+	this.train = function(inputs, label){
+		var currentGuess = this.guess(inputs);
+		var error = label - currentGuess;
+
+		for(var i = 0; i < weights.length; i++){
+			//0.1 is learning rate
+			weights[i] += error * inputs[i] * 0.1;
+
+		}
+
+
+
+	}
 
 
 }
--- Neural/SPerc.js
+++ Neural/SPerc.js
@@ -2,12 +2,13 @@ var data = [];
 
 
 
-var weights = [];
+
 
 var points = new Array(100);
+var neural = new Perceptron(2);
 function setup(){
   createCanvas(800,800);
-	randomWeights();
+	neural.randomWeights();
 	randomPoints();
 }
 
@@ -24,42 +25,11 @@ function randomPoints(){
 
 }
 
-function randomWeights(){
-	for(var i = 0; i < 2; i++){
-		weights.push(random(-1,1));
-		console.log(weights[i]);
-	}
-
-
-}
-
-function guess(inputs){
-	var sum = 0;
-	for(var i = 0; i < weights.length; i++)
-		sum+= inputs[i] * weights[i];
-		//activation function is a function of the sign of the sum
-		var output = sum >= 0 ? 1 : -1;
-		return output;
-}
-
-function train(inputs, label){
-	var currentGuess = guess(inputs);
-	var error = label - currentGuess;
-
-	for(var i = 0; i < weights.length; i++){
-		//0.1 is learning rate
-		weights[i] += error * inputs[i] * 0.1;
-
-	}
-
-
-
-}
 
 function mousePressed(){
 	console.log("oh");
 	for(var i = 0; i < points.length; i++){
-		train([points[i][0], points[i][1]], points[i][2]);
+		neural.train([points[i][0], points[i][1]], points[i][2]);
 
 
 
@@ -78,7 +48,7 @@ function draw(){
       stroke(255);
       ellipse(points[i][0], points[i][1], 6,6);
 
-			var nxt = guess(points[i]);
+			var nxt = neural.guess(points[i]);
 			//console.log("guess/label " + nxt + " | " + points[i][2]);
 			if(nxt == points[i][2])fill(0,255,0);
 			else if(nxt == points[i][2] * -1) fill(255,0,0);
