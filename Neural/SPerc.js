b1624d05877496b64249d60f01efb071460b871d
Luke <lr98lr@aol.com>
2018-06-30 18:58:13 -0400

    trained/learns and fits new data to set correctly

--- Neural/SPerc.js
+++ Neural/SPerc.js
@@ -4,19 +4,14 @@ var data = [];
 
 var weights = [];
 
-var points = new Array(50);
+var points = new Array(100);
 function setup(){
   createCanvas(800,800);
 	randomWeights();
-
-	var inputs = [-1, 0.5];
-	var g = guess(inputs);
 	randomPoints();
 }
 
-function getRndInteger(min, max) {
-    return Math.floor(random(min, max)) + min;
-}
+
 
 
 function randomPoints(){
@@ -24,15 +19,17 @@ function randomPoints(){
 		var rx = random(0, width);
 		var ry = random(0, height);
 		points[i] = [rx, ry, 0];
-		points[i][2] = points[i][1] > points[i][0] ? 1 : 0;
-		console.log(points[i][0] + ", " + points[i][1] + ", " + points[i][2])
+		points[i][2] = points[i][1] > points[i][0] ? 1 : -1;
 	}
 
 }
 
 function randomWeights(){
-	for(var i = 0; i < 2; i++)
+	for(var i = 0; i < 2; i++){
 		weights.push(random(-1,1));
+		console.log(weights[i]);
+	}
+
 
 }
 
@@ -45,16 +42,55 @@ function guess(inputs){
 		return output;
 }
 
+function train(inputs, label){
+	var currentGuess = guess(inputs);
+	var error = label - currentGuess;
+
+	for(var i = 0; i < weights.length; i++){
+		//0.1 is learning rate
+		weights[i] += error * inputs[i] * 0.1;
+
+	}
+
+
+
+}
+
+function mousePressed(){
+	console.log("oh");
+	for(var i = 0; i < points.length; i++){
+		train([points[i][0], points[i][1]], points[i][2]);
+
+
+
+	}
+
+}
+
 function draw(){
     background(0);
+		stroke(255);
+		line(0, 0,width, height);
     for(var i = 0; i < points.length; i++){
-      if(points[i][2] == 0) stroke(255,0,0);
-			else stroke(0,255,0);
+      if(points[i][2] == -1) fill(255);
+			else fill(0);
 
-      fill(0);
-      ellipse(points[i][0], points[i][1], 8,8);
+      stroke(255);
+      ellipse(points[i][0], points[i][1], 6,6);
+
+			var nxt = guess(points[i]);
+			//console.log("guess/label " + nxt + " | " + points[i][2]);
+			if(nxt == points[i][2])fill(0,255,0);
+			else if(nxt == points[i][2] * -1) fill(255,0,0);
+			else fill(0,0,255);
+			ellipse(points[i][0], points[i][1], 8,8);
 
     }
+
+
+
+
+
 
 
 
