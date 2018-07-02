b9a2bd05315ecd186c2e31acad57df4f7a1f8b3a
Luke <lr98lr@aol.com>
2018-07-02 15:55:40 -0400

    feed forward done

--- MLNeural/MLNeuralNet.js
+++ MLNeural/MLNeuralNet.js
@@ -16,7 +16,7 @@ class NeuralNet{
 		this.bias[0] = math.matrix();
 		this.bias[0].resize([this.numHiddenNodes, 1]);
 		this.bias[1] = math.matrix();
-		this.bias[1].resize([this.numInputs, 1]);
+		this.bias[1].resize([this.numOutputs, 1]);
 
 
 	}
@@ -31,35 +31,25 @@ class NeuralNet{
 	}
 
 	randomizeWeights(){
-
-
-		for(let i = 0; i < this.weights.length; i++){
-			for(let k = 0; k < this.weights[i].size()[0]; k++){
-				for(let j = 0; j < this.weights[i].size()[1]; j++){
-					this.weights[i].subset(math.index(k,j), random(-1,1));
-				//	console.log(this.weights[i].get([k,j]) + ", ");
-				}
-				//console.log('\n');
-			}
-			print(i + ": " + this.weights[i].toString());
-
-		}
-
+		for(let i = 0; i < this.weights.length; i++)
+		for(let k = 0; k < this.weights[i].size()[0]; k++)
+		for(let j = 0; j < this.weights[i].size()[1]; j++)
+			this.weights[i].subset(math.index(k,j), random(-1,1));
 	}
 
 	feedforward(input){
 
-
-
-		//let hiddenLayer = math.matrix();
-	//	hiddenLayer.resize([this.numHiddenNodes, 1]);
+		//hidden layer sum
 		let hiddenLayer = math.matrix(math.multiply(this.weights[0], input));
 		hiddenLayer.resize([3,1]);
-		print(hiddenLayer.toString() + ": SIZE: " + hiddenLayer.size());
 		math.add(hiddenLayer, this.bias[0]);
-	  print(hiddenLayer.toString() + ": SIZE: " + hiddenLayer.size());
 		this.sigmoid(hiddenLayer);
-		print(hiddenLayer.toString() + ": SIZE: " + hiddenLayer.size());
+
+		//output layer
+		let outputLayer = math.matrix(math.multiply(this.weights[1], hiddenLayer));
+		math.add(outputLayer, this.bias[1]);
+		this.sigmoid(outputLayer);
+		return outputLayer.subset(math.index(0, 0));
 
 	}
 
