8a95f114b479daf005279ddc197e1ec56fbf465a
Luke <lr98lr@aol.com>
2018-07-02 13:03:05 -0400

    started ground up + added math.js

---
+++ MLNeural/MLNNCore.js
@@ -0,0 +1,16 @@
+var net;
+
+
+
+function setup(){
+
+	net = new NeuralNet(3,3,1);
+
+
+}
+
+
+function draw(){
+
+
+}
---
+++ MLNeural/MLNeuralNet.js
@@ -0,0 +1,8 @@
+
+function NeuralNet(numInputs, numHidden, numOutputs){
+	this.inputs = numInputs;
+	this.hiddenNodes = numHidden;
+	this.outputs = numOutputs;
+
+
+}
