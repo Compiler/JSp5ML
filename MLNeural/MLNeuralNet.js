3d0ec798defc9b6fe1079601dc2da6ba94298e89
Luke <lr98lr@aol.com>
2018-07-02 15:41:10 -0400

    hidden layer math... took way too long mathjs is fun

--- MLNeural/MLNeuralNet.js
+++ MLNeural/MLNeuralNet.js
@@ -9,7 +9,7 @@ class NeuralNet{
 		this.weights[0].resize([this.numHiddenNodes, this.numInputs]);
 		this.weights[1] = math.matrix();
 		this.weights[1].resize([this.numOutputs, this.numHiddenNodes]);
-
+		this.randomizeWeights();
 
 
 		this.bias = new Array(this.weights.length);
@@ -19,14 +19,13 @@ class NeuralNet{
 		this.bias[1].resize([this.numInputs, 1]);
 
 
-
 	}
 
 	sigmoid(input){
 		for(let k = 0; k < input.size()[0]; k++){
 			for(let j = 0; j < input.size()[1]; j++){
 				let curVal = input.get([k,j]);
-				input.subset(math.index(k,j), math.pow(math.e, curVal) / (math.pow(math.e, curVal) + 1));
+				input.subset(math.index(k,j), 1 / (math.pow(math.e, -curVal) + 1));
 			}
 		}
 	}
@@ -38,22 +37,39 @@ class NeuralNet{
 			for(let k = 0; k < this.weights[i].size()[0]; k++){
 				for(let j = 0; j < this.weights[i].size()[1]; j++){
 					this.weights[i].subset(math.index(k,j), random(-1,1));
+				//	console.log(this.weights[i].get([k,j]) + ", ");
 				}
+				//console.log('\n');
 			}
+			print(i + ": " + this.weights[i].toString());
 
 		}
 
 	}
 
 	feedforward(input){
-		let inputMatrix = math.matrix();
-		inputMatrix.resize([input.length, 1]);
-		let hiddenLayer = math.matrix();
+
+
+
+		//let hiddenLayer = math.matrix();
+	//	hiddenLayer.resize([this.numHiddenNodes, 1]);
+		let hiddenLayer = math.matrix(math.multiply(this.weights[0], input));
 		hiddenLayer.resize([3,1]);
-		math.multiply(hiddenLayer, math.transpose(inputMatrix));
+		print(hiddenLayer.toString() + ": SIZE: " + hiddenLayer.size());
 		math.add(hiddenLayer, this.bias[0]);
+	  print(hiddenLayer.toString() + ": SIZE: " + hiddenLayer.size());
 		this.sigmoid(hiddenLayer);
+		print(hiddenLayer.toString() + ": SIZE: " + hiddenLayer.size());
 
 	}
+
+
+
+
+
+
+
+
+
 
 }
