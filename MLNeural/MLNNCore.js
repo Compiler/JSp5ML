5ca8b7995f97d24498706f6e74c08a3756bbf37a
Luke <lr98lr@aol.com>
2018-07-05 23:01:27 -0400

    final visual stuff

--- MLNeural/MLNNCore.js
+++ MLNeural/MLNNCore.js
@@ -4,9 +4,11 @@ var inputNodes, hiddenNodes, outputNodes;
 var offsets = new Array(3);
 function setup(){
 	createCanvas(1080,800);
-	inputNodes = 3; hiddenNodes = 7; outputNodes = 1;
+	inputNodes = 4; hiddenNodes = 7; outputNodes = 2;
+	let input = new Array(inputNodes);
+	for(let m = 0; m < inputNodes; m++) input[m] = random();
 	net = new NeuralNet(inputNodes, hiddenNodes,outputNodes);
-	net.feedforward([1,2,3]);
+	net.feedforward(input);
 	offsets[0] = (2 * (height/3)) / inputNodes;
 	offsets[1] = (height) / hiddenNodes;
 	offsets[2] = (height/3) / outputNodes;
@@ -20,6 +22,9 @@ function setup(){
 
 	for(let i = 0; i < hiddenNodes; i++)
 		this.points[1][i] = new Point(550, (height - sz ) - (i * offsets[1]) - sz, sz);
+
+	for(let i = 0; i < outputNodes; i++)
+		this.points[2][i] = new Point(850, (height / outputNodes) + (i * offsets[2]) - sz * 3, sz);
 }
 
 let sz = 30;
@@ -28,13 +33,18 @@ function draw(){
 	stroke(255);
 	fill(0);
 
-	for(let i = 0; i < 2; i++)
+	for(let i = 0; i < 3; i++)
 		for(let k = 0; k < this.points[i].length; k++){
 			this.points[i][k].draw();
 		}
-	for(let i = 0; i < 3; i++)
+	for(let i = 0; i < inputNodes; i++)
 		for(let k = 0; k < hiddenNodes; k++){
 			line(this.points[0][i].getX() + sz / 2,this.points[0][i].getY(),this.points[1][k].getX() - sz / 2,this.points[1][k].getY());
+		}
+
+	for(let i = 0; i < hiddenNodes; i++)
+		for(let k = 0; k < outputNodes; k++){
+			line(this.points[1][i].getX() + sz / 2,this.points[1][i].getY(),this.points[2][k].getX() - sz / 2,this.points[2][k].getY());
 		}
 
 
